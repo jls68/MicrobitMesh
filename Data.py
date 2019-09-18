f78075ca83525296ca4b8772849d290881907ef9
tmmb1 <tmmb1@students.waikato.ac.nz>
2019-09-19 10:27:51 +1200

    Forgot to add the updated Ped Python file oops

--- Data.py
+++ Data.py
@@ -25,26 +25,28 @@ posy = 0
 posFail = 0
 lastRead = 0
 
-#ab = bytearray()
-#ab.append(0x54)
-#ab.append(0x41)
-#ab.append(0x4D)
-#ab.append(0x41)
-#ab.append(0x48)
-#ab.append(0x41)
-#ab.append(0x55)
-#ab.append(0x80)
-#ab.append(0x80)
-
+'''
+ab = bytearray()
+ab.append(0x54)
+ab.append(0x41)
+ab.append(0x4D)
+ab.append(0x41)
+ab.append(0x48)
+ab.append(0x41)
+ab.append(0x55)
+ab.append(0x80)
+ab.append(0x80)
+ab.append(0x80)
+'''
 while (True):
- xy = ser.read(8)
- if (xy.__len__() == 8):
-
+  xy = ser.read(12)
+  print(xy);
+  if (xy.__len__() == 12):
    #While it has not found an empty value
    while(posFail != 1):
      #Keeps iterating if there is no empty value
-     if(xy[pos] == 0x00):
-       pos = (pos + 1) % 8
+     if(xy[pos] == 0xff):
+       pos = (pos + 1) % 12
        if(lastRead == 1):
          posFail = 1
        lastRead = 1
@@ -52,24 +54,24 @@ while (True):
      #Otherwise is not 0x80
      else:
        lastRead = 0
-       pos = (pos + 1) % 8
+       pos = (pos + 1) % 12
 
-   pos = pos % 8
-   posy = (pos + 1) % 8
+   pos = pos % 12
+   posy = (pos + 1) % 12
    if (xy[posy] >= 128):
-     newsy = -(32768 * 2 - (xy[posy] * 256 + xy[pos])) / 3
+     newsx = -(32768 * 2 - (xy[posy] * 256 + xy[pos])) / 3
    else:
-     newsy = (xy[posy] * 256 + xy[pos]) / 3
+     newsx = (xy[posy] * 256 + xy[pos]) / 3
 
-   if (xy[(posy+2)%8] >= 128):
-     newsx = -(32768 * 2 - (xy[(posy+2)%8] * 256 + xy[(pos+2)%8])) / 3
+   if (xy[(posy+2)%12] >= 128):
+     newsy = -(32768 * 2 - (xy[(posy+2)%12] * 256 + xy[(pos+2)%12])) / 3
    else:
-     newsx = (xy[(posy+2)%8] * 256 + xy[(pos+2)%8]) / 3
+     newsy = (xy[(posy+2)%12] * 256 + xy[(pos+2)%12]) / 3
 
-   if (xy[(posy+4)%8] >= 128):
-     newsz = -(32768 * 2 - (xy[(posy+4)%8] * 256 + xy[(pos+4)%8])) / 3
+   if (xy[(posy+4)%12] >= 128):
+     newsz = -(32768 * 2 - (xy[(posy+4)%12] * 256 + xy[(pos+4)%12])) / 3
    else:
-     newsz = (xy[(posy+4)%8] * 256 + xy[(pos+4)%8]) / 3
+     newsz = (xy[(posy+4)%12] * 256 + xy[(pos+4)%12]) / 3
 
    #print(newsy, end=" ", flush=True)
    #print(newsx, end=" ", flush=True)
@@ -78,6 +80,10 @@ while (True):
    print("\nX position: " + str(newsx))
    print("Y position " + str(newsy))
    print("Z position: " + str(newsz))
+
+   #TODO: Get magnitude of the accelerometers and display the data on the graph as one. To discuss
+   mag = (x * x + y * y + z * z)
+
    sx = sx + 1
    sz = newsx
    sy = newsy
@@ -87,5 +93,3 @@ while (True):
    if (sx > 800):
      sx = 0
      canvas.delete("all")
-   
-
